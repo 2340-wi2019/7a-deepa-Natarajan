0019024e90507adffa88be7946c1cd5db282fb62
deepa-Natarajan <53449985+deepa-Natarajan@users.noreply.github.com>
2020-02-02 03:34:54 -0600

    Complete

---
+++ assignment07/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Assignment 7  </title>
+<meta charset="utf-8">
+</head>
+<body>
+<!--Deepa NAtarajan
+    assignment07
+    19WI_INFO_2340_WW - Online - Internet Scripting
+    Instructor: Ms.Lisa Thoendel
+    -->
+<?php 
+
+$dinner='Meatloaf ';
+$star_rate=3;
+$Fav_Place='Zoo';
+$Fav_Animal='Chickenhawk';
+
+
+?>
+<h1>Good Morning, Nebraska!</h1>
+<div>Last night I ate <?php echo $dinner;?>.</div><br>
+<div>While it was good, it wasn't perfect.I'd give <?php echo $star_rate; ?>  stars out of 4.</div><br>
+<div>Tomorrow I plan to go to the <?php echo $Fav_Place; ?>.</div><br>
+<div>My favorite animal is the <?php echo $Fav_Animal; ?>.</div><br>
+
+
+
+
+
+</body>
+
+</html>
+
+
+
+
+
+
